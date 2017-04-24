c305064c55dd83779dbf07f14f4006b1856919dd
Kenneth <kenneth@creative-mischief.com>
2017-04-23 22:06:26 -0400

    NYT

---
+++ NYT/assets/js/main.js
@@ -0,0 +1,51 @@
+console.log("Hello World from main.js!");
+
+function populate(result) {
+	var apiResult = result.response.docs;
+	for (var i = $('div').length - 1; i >= 0; i--) {
+		$('div')[i].parentNode.removeChild($('div')[i]);
+	}
+	for (var i = apiResult.length - 1; i >= 0; i--) {
+		var string = apiResult[i].pub_date;
+		var title = document.createElement('a');
+		var summary =document.createElement('p');
+		var date = document.createElement('h4');
+		var container = document.createElement('div');
+		var newString = '';
+		$(title).html(apiResult[i].headline.main);
+		$(title).attr({
+		    target:"_blank", 
+		    href:apiResult[i].web_url
+		});
+        string.split();
+        for (var j = 0; j < 10; j++) {
+        	newString +=string[j];
+        }
+		$(summary).html(apiResult[i].lead_paragraph)
+		$(date).html(newString);
+		$(container).append(title,summary,date);
+		$('body').append(container);
+	}
+
+
+}
+
+$('form').on("submit", function(e){
+	e.preventDefault();
+	var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+	url += '?' + $.param({
+	  'api-key': "f47d20e5ae074e81bc49580a8b0f67db",
+	  'q': $('input').val(),
+	  'sort': "newest"
+	});
+	$.ajax({
+	  url: url,
+	  method: 'GET',
+	}).done(function(result) {
+	  console.log(result);
+	  populate(result);
+	}).fail(function(err) {
+	  throw err;
+	});
+})
+
